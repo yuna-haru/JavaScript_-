dabc144562571ad03fe68fe061edfe9c9f16e17d
unknown <100776483+yuna-haru@users.noreply.github.com>
2022-08-14 20:34:46 +0900

    js if文

---
+++ app.js
@@ -0,0 +1,28 @@
+let hello = 'Hello World';
+alert(hello);
+let init1 = 1;
+alert(init1);
+let str1 = 'JavaScriptを覚えよう'
+alert(str1);
+
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let str3 = 'Hello';
+let str4 = 'World!!';
+alert(str3 + str4);
+
+let orange = 140
+let apple = 120
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else {
+  alert('みかんの値段がりんごより高い');
+}
