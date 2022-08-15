2de57064dd374c0339db9af9046b7ee3f1b7671b
unknown <100776483+yuna-haru@users.noreply.github.com>
2022-08-15 21:28:52 +0900

    関数複数

--- app4.js
+++ app4.js
@@ -11,6 +11,26 @@ function addString(strA){
 let promptStr = prompt('何か好きな文字を入力してください。');
 alert(promptStr);
 
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+// alert('あなたの選んだ手は' + user_hand + 'です。');
+
+
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
-alert('あなたの選んだ手は' + user_hand + 'です。');
+let js_hand = getJShand();
+let judge = winLose(user_hand, js_hand);
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。')
+
+function getJAhand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+  
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  }else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+  return hand_name
+}
 
