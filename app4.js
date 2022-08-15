605bd143f08d3b7b724d2c52cd74d4177b7cb40a
unknown <100776483+yuna-haru@users.noreply.github.com>
2022-08-15 16:57:15 +0900

    関数prompt

--- app4.js
+++ app4.js
@@ -4,6 +4,13 @@ alertString = addString("WebCamp");
 alert(alertString);
 
 function addString(strA){
-  let addStr = "Hello" +strA;
+  let addStr = "Hello" + strA;
   return addStr;
 }
+
+let promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+alert('あなたの選んだ手は' + user_hand + 'です。');
+
